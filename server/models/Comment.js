3a8a141cd3018f2e59fd7f75ac50c613a8bef053
saifwitty <saifwitty5@gmail.com>
2025-12-06 06:27:36 +0600

    adding comments

---
+++ server/models/Comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  blogId: { type: String, required: true },
+  username: { type: String, required: true },
+  comment: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
